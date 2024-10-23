f03ec0583be00a11b7ddd1ba74d89a8b634afe97
root <root@gamejam.homelab.lan>
2024-10-23 16:27:58 +0000

    nextjs gamejam

---
+++ src/app/layout.js
@@ -0,0 +1,31 @@
+import localFont from "next/font/local";
+import "./globals.css";
+import Image from "next/image";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: "WinterJam - Hosted by IPMAIA",
+  description: "Gamejam hosted by IPMAIA from game dev students, for game dev students",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+        </body>
+    </html>
+  );
+}
