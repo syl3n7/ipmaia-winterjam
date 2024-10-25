bd634d15e30dc13202be985cfa042bc1ef79b9ef
Syl3nt ~ Cláudio <claudio.silva365@gmail.com>
2024-10-25 21:03:43 +0100

    changed rules page
    added footer to all pages
    trying custom ipmaia svg
    added template for rules.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,6 +8,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["InterVariables", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
