eded02db0f17f38e7a5ea93b20c0a3cda48d7b40
Syl3nt ~ Cláudio <claudio.silva365@gmail.com>
2024-10-30 19:36:44 +0000

    fixed error on year function

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div className="flex flex-col justify-between pt-5 pb-10 border-t border-gray-800 sm:flex-row">
           <p className="text-sm text-gray-500">
-            © Copyright {<Year/>} IPMAIA  Todos direitos reservados.
+            © Copyright <Year/> IPMAIA  Todos direitos reservados.
           </p>
           <div className="flex items-center mt-4 space-x-4 sm:mt-0">
             <a
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,20 +8,15 @@ const MainNavbar = () => {
     <Navbar fluid 
     className="bg-[#d86f3a] sticky top-0 z-50 shadow-lg">
       <NavbarBrand as={Link} href="/">
-        <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+        <img src="../../favicon.svg" className="mr-3 h-6 sm:h-9" alt="IPMAIA WinterJam" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
       </NavbarBrand>
       <NavbarToggle />
       <NavbarCollapse>
-        <NavbarLink href="#" active>
-          Home
-        </NavbarLink>
-        <NavbarLink as={Link} href="#">
-          About
-        </NavbarLink>
-        <NavbarLink href="#">Services</NavbarLink>
-        <NavbarLink href="#">Pricing</NavbarLink>
-        <NavbarLink href="#">Contact</NavbarLink>
+        <NavbarLink href="#" active>Home</NavbarLink>
+        <NavbarLink as={Link} href="#">Quem Somos?</NavbarLink>
+        <NavbarLink href="/rules">Regras</NavbarLink>
+        <NavbarLink href="mailto:gamejam.at.ipmaia@gmail.com">Contact</NavbarLink>
       </NavbarCollapse>
     </Navbar>
   );
