949d13b2fbb74af792e588e2d1fd931ef1aa7dc0
syl3n7 <a040516@ipmaia.pt>
2026-02-20 14:19:44 +0000

    remove _document.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,6 +31,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://rsms.me" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen flex flex-col`}>
